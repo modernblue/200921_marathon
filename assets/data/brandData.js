5670754d069a265c8e13ecfe5937ff079d337634
modernblue <modernblue.okada@gmail.com>
2020-09-18 08:42:06 +0900

    first commit.

---
+++ assets/data/brandData.js
@@ -0,0 +1,62 @@
+brands([
+  {
+    "id": 1,
+    "brand": "ACNE STUDIOS",
+    "kana": "アクネストゥディオズ",
+    "sex": "m"
+  },
+  {
+    "id": 2,
+    "brand": "BALENCIAGA",
+    "kana": "バレンシアガ",
+    "sex": "m"
+  },
+  {
+    "id": 3,
+    "brand": "BOTTEGA VENETA",
+    "kana": "ボッテガ・ヴェネタ",
+    "sex": "m"
+  },
+  {
+    "id": 4,
+    "brand": "GUCCI",
+    "kana": "グッチ",
+    "sex": "m"
+  },
+  {
+    "id": 5,
+    "brand": "BALENCIAGA",
+    "kana": "バレンシアガ",
+    "sex": "f"
+  },
+  {
+    "id": 6,
+    "brand": "BOTTEGA VENETA",
+    "kana": "ボッテガ・ヴェネタ",
+    "sex": "f"
+  },
+  {
+    "id": 7,
+    "brand": "SAINT LAURENT PARIS",
+    "kana": "サン・ローラン",
+    "sex": "f"
+  },
+  {
+    "id": 8,
+    "brand": "TATRAS",
+    "kana": "タトラス",
+    "sex": "f"
+  },
+  {
+    "id": 9,
+    "brand": "WEEKEND MAX MARA",
+    "kana": "ウィークエンドマックスマーラ",
+    "sex": "f"
+  },
+  {
+    "id": 10,
+    "brand": "WOOLRICH",
+    "kana": "ウールリッチ",
+    "sex": "f"
+  }
+]);
